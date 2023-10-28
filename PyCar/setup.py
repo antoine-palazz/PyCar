f52355af43e9da7db2e35c81f1ce151a4468e498
AugustinCablant <123320681+AugustinCablant@users.noreply.github.com>
2023-10-28 17:39:15 +0200

    PyCar

---
+++ PyCar/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup 
+
+setup(
+    name = "PyCar"
+    version = "0.0.1"
+    author = "Cablant Augustin & Khelifa Naïl & Pogu Thomas"
+    author_email = "augustin.cablant@ensae.fr"
+    url = ""
+)
